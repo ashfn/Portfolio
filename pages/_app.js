aabd711005c24f9e85c6483272a29f6d2fd4030c
auth <65650599+Authorises@users.noreply.github.com>
2023-01-15 00:08:30 +0000

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div data-theme="authorises" className="bg-slate-900">
+      <Analytics />
       <Component {...pageProps} />
     </div>
   ) 
